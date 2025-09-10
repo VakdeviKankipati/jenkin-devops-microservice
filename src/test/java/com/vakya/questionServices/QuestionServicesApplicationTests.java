269dbdf41ccfbfee95816df497c62a76fdcd4108
Kankipati Vakdevi <124621621+VakdeviKankipati@users.noreply.github.com>
2025-09-10 12:38:37 +0530

    Update QuestionServicesApplicationTests.java

--- src/test/java/com/vakya/questionServices/QuestionServicesApplicationTests.java
+++ src/test/java/com/vakya/questionServices/QuestionServicesApplicationTests.java
@@ -1,14 +1,28 @@
 package com.vakya.questionServices;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-@ActiveProfiles("test")   // tells Spring to load application-test.properties
+@ActiveProfiles("test")
 class QuestionServicesApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    void applicationContextHasRequiredBeans() {
+        // Add assertions for your specific beans
+        assertThat(applicationContext.containsBean("questionServicesApplication")).isTrue();
     }
 }
